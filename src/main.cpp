1c97e7f0d83a0ff03c564b453a50d50e09fe9b63
BBarisKilic <bulentbariskilic@gmail.com>
2021-02-07 00:18:13 +0300

    Fourth Commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,45 @@ int loopCounter = 0;
 
 void setup()
 {
-  BTserial.begin(9600); 
-  Serial.begin(9600);
+  //Starting software serial at 9600 Baudrate
+  BTserial.begin(9600);
 }
 
 void loop()
 {
   loopCounter++;
 
-	if(BTserial.available()>0)
+  if (BTserial.available() > 0)
   {
-    //If you send not just a char but a whole word from the paired device, you must change "read" to "readString" and type of the variable "data" to String. (e.g. data +=BTserial.readString())
-		data = BTserial.read();
+    //If you send not just a char but a whole word from the paired device, you must change "read" to "readString" and type of the variable "data" to String.
+    //e.g. data +=BTserial.readString()
+    data = BTserial.read();
   }
 
-  //Every second Arduino will send data to other device.
+  //Every second the Arduino will send data to other device. You can set a new frequency with changing not only "loopCounter" but also delay time.
   if (loopCounter == 4)
   {
     loopCounter = 0;
-    if (data == 'a'){
+    if (data == 'a')
+    {
       //It is important to use "println" instead of "print" or "write"!
       BTserial.println("pressed a!");
-	  } else if (data == 'b'){
+    }
+    else if (data == 'b')
+    {
       //It is important to use "println" instead of "print" or "write"!
       BTserial.println("pressed b!");
-	  } else if(data == 'c'){
+    }
+    else if (data == 'c')
+    {
       //It is important to use "println" instead of "print" or "write"!
       BTserial.println("pressed c!");
-	  } else{
+    }
+    else
+    {
       //It is important to use "println" instead of "print" or "write"!
-	  	BTserial.println("no match!");
-	  }
+      BTserial.println("no match!");
+    }
   }
-   delay(250);
+  delay(250);
 }
